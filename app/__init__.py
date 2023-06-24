269b6f0dd8dde922c8c70319cbbdb4a95f80bf5c
AnnijaKaupere <annijakaupere@gmail.com>
2023-06-24 16:05:23 +0300

    authentication

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from config import Config
 from app.views import bp
 from app.db import db
 from app.models import Employee, Position, Division, Job # noqa
+from app.auth import auth
+from flask_login import LoginManager
+from app.models import User
 
 
 app = Flask(__name__)
@@ -14,5 +17,16 @@ app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 # register blueprint here
 app.register_blueprint(bp)
+app.register_blueprint(auth)
